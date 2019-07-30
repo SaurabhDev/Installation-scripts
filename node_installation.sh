18b8304650393c262a74bf4d94d7b7a8f72b6aaa
SaurabhDev <singhalsaurabh20@gmail.com>
2019-07-30 11:11:03 +0530

    Added node installation

---
+++ node_installation.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt-get install npm
+nodejs -v
+sudo apt-get install nodejs
+npm -v
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+nodejs -v
+npm -v
+sudo npm install -g n
+node -v
+history
