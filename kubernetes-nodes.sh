124b0cc55e125ee1a12c73484f419b1a15524685
Saurabh <singhalsaurabh20@gmail.com>
2021-01-26 03:33:46 +0530

    Update kubernetes-nodes.sh

--- kubernetes-nodes.sh
+++ kubernetes-nodes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-UBUNTU_VERSION=18.04
-K8S_VERSION=1.11.3-00
-node_type=master
+#UBUNTU_VERSION=18.04
+#K8S_VERSION=1.11.3-00
+#node_type=master
 
 #Update all installed packages.
 apt-get update -y
@@ -10,22 +10,15 @@ apt-get upgrade -y
 
 #if you get an error similar to
 #'[ERROR Swap]: running with swap on is not supported. Please disable swap', disable swap:
-sudo swapoff -a
+#sudo swapoff -a
 
 # install some utils
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
 #Install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
-if [ $UBUNTU_VERSION == "16.04" ]; then
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
-elif [ $UBUNTU_VERSION == "18.04" ]; then
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-else
-    #default tested version
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
-fi
 sudo apt-get update
 sudo apt-get install -y docker.io
 
@@ -36,11 +29,18 @@ sudo apt-get install -y nfs-common
 sudo systemctl enable docker.service
 
 #Update the apt source list
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] http://apt.kubernetes.io/ kubernetes-xenial main"
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
 
 #Install K8s components
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+
+
 
 echo "Done."
