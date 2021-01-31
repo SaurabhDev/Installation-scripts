7996b33d46620cac3a54332058042d0f764ce4d6
Saurabh <singhalsaurabh20@gmail.com>
2021-01-31 23:01:23 +0530

    Update kubernetes-master.sh

--- kubernetes-master.sh
+++ kubernetes-master.sh
@@ -35,20 +35,25 @@ sudo apt-get install -y nfs-common
 #Enable docker service
 sudo systemctl enable docker.service
 
+######### Copy Line 40-42 and run as one command ###########
 #Kubernetes installation begins
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
 br_netfilter
 EOF
 
+######### Copy Line 44-46 and run as one command ###########
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
+
 sudo sysctl --system
 
 #Update the apt source list
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+######### Copy Line 54-56 and run as one command ###########
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
